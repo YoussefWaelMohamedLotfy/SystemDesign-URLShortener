fbaef53a1e744886e2c1edd4b5f3e7acf7d70dac
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-03-23 21:14:30 +0200

    Configured Project Initial Files

---
+++ SystemDesign-URLShortener/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Swashbuckle.AspNetCore.Filters;
+using System.Reflection;
+
+namespace SystemDesign_URLShortener;
+
+public class Startup
+{
+    internal IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        services.AddAutoMapper(typeof(Startup));
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(swaggerOptions =>
+        {
+            swaggerOptions.ExampleFilters();
+
+            string xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            swaggerOptions.IncludeXmlComments(xmlPath);
+
+            swaggerOptions.UseApiEndpoints();
+        });
+
+        services.AddSwaggerExamplesFromAssemblyOf<Startup>();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment() || env.IsStaging())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
