52fc8463a4d1ae751a5a79289e183effd05b03a3
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-03-23 22:14:54 +0200

    Updated POST Request for Strong-Typed Requests

--- SystemDesign-URLShortener/Endpoints/V1/URLs/Shorten.cs
+++ SystemDesign-URLShortener/Endpoints/V1/URLs/Shorten.cs
@@ -1,15 +1,17 @@
 ﻿using Ardalis.ApiEndpoints;
 using Microsoft.AspNetCore.Mvc;
+using URLShortener.Core.Commands;
+using URLShortener.Core.Results;
 
 namespace SystemDesign_URLShortener.Endpoints.V1.URLs;
 
-public class Shorten : EndpointBaseSync.WithRequest<string>.WithActionResult
+public class Shorten : EndpointBaseSync.WithRequest<ShortenerCommand>.WithActionResult<ShortenerResult>
 {
 
 
     [HttpPost("/api/shorten")]
-    public override ActionResult Handle(string longUrl)
+    public override ActionResult<ShortenerResult> Handle(ShortenerCommand request)
     {
-        return Ok(new { longUrl = longUrl });
+        return Ok(new ShortenerResult { ID = 5, LongUrl = request.LongUrl, ShortUrl = "Short Version"});
     }
 }
