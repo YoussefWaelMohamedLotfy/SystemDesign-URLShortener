4d9a33b220ae8972f759614f6e4abeeceea0fef5
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-03-23 21:53:02 +0200

    Created Endpoints for Shortener and Redirection

---
+++ SystemDesign-URLShortener/Endpoints/V1/URLs/Get.cs
@@ -0,0 +1,18 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SystemDesign_URLShortener.Endpoints.V1.URLs;
+
+public class Get : EndpointBaseSync.WithRequest<string>.WithActionResult
+{
+
+
+
+    [HttpGet("/api/{shortUrl}", Name = "Redirecter")]
+    public override ActionResult Handle(string shortUrl)
+    {
+        return Redirect("https://google.com");
+    }
+
+    
+}
---
+++ SystemDesign-URLShortener/Endpoints/V1/URLs/Shorten.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SystemDesign_URLShortener.Endpoints.V1.URLs;
+
+public class Shorten : EndpointBaseSync.WithRequest<string>.WithActionResult
+{
+
+
+    [HttpPost("/api/shorten")]
+    public override ActionResult Handle(string longUrl)
+    {
+        return Ok(new { longUrl = longUrl });
+    }
+}
