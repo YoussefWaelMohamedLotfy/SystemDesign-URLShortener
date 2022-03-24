737c4274408e9f827fcca9d9639d3fd4ca8c2f8e
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-03-24 21:38:27 +0200

    Fixed Base62 Conversion

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-SystemDesign-URLShortener
+# URL Shortener - System Design Interview
+
+> This repository is my solution for the problem in [Chapter 8 in System Design Interview (Second Edition) by Dr. Alex Xu](https://printige.net/product/system-design-interview-an-insider-guide/)
--- SystemDesign-URLShortener/Endpoints/V1/URLs/Shorten.cs
+++ SystemDesign-URLShortener/Endpoints/V1/URLs/Shorten.cs
@@ -2,6 +2,7 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using SystemDesign_URLShortener.Data;
+using SystemDesign_URLShortener.Helpers;
 using URLShortener.Core.Commands;
 using URLShortener.Core.Entities;
 using URLShortener.Core.Results;
@@ -24,27 +25,12 @@ public class Shorten : EndpointBaseAsync.WithRequest<ShortenerCommand>.WithActio
     {
         var url = _mapper.Map<URL>(request);
         int maxID = _context.URLs.Select(x => x.ID).Max();
-        url.ShortUrl = ToBase62(maxID + 1);
+        url.ShortUrl = Base62Converter.EncodeUInt64((ulong)maxID + 1);
 
         await _context.URLs.AddAsync(url, cancellationToken: cancellationToken);
         await _context.SaveChangesAsync(cancellationToken);
 
         var result = _mapper.Map<ShortenerResult>(url);
         return Ok(result);
-    }
-
-    private string ToBase62(int number)
-    {
-        ReadOnlySpan<char> alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        int basis = 62;
-        var res = string.Empty;
-
-        while (number > 0)
-        {
-            res = string.Concat(alphabet[number % basis], res);
-            number /= basis;
-        }
-
-        return res;
     }
 }
