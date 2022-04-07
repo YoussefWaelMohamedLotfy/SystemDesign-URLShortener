350187b703b84a5c13650bdd2454288794a911f6
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-04-07 03:48:53 +0200

    Added SDK for URL Shortener

---
+++ SDKs/URLShortener.SDK/IUrlApi.cs
@@ -0,0 +1,17 @@
+﻿using Refit;
+using URLShortener.Core.Commands;
+using URLShortener.Core.Results;
+
+namespace URLShortener.SDK;
+
+public interface IUrlApi
+{
+    [Get("/api/{shortUrl}")]
+    Task RedirectURL(string shortUrl);
+
+    [Post("/api/shorten")]
+    Task<ApiResponse<ShortenerResult>> Base62Shorten([Body] ShortenerCommand requestBody);
+
+    [Post("/api/shorten/hash")]
+    Task<ApiResponse<ShortenerResult>> MD5Shorten([Body] ShortenerCommand requestBody);
+}
---
+++ SDKs/URLShortener.SDK/SDKServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Refit;
+
+namespace URLShortener.SDK;
+
+public static class SDKServiceRegistration
+{
+    public static void AddUrlShortener(this IServiceCollection services)
+    {
+        services.AddRefitClient<IUrlApi>()
+            .ConfigureHttpClient(c =>
+            {
+                c.BaseAddress = new("https://localhost:7261");
+            });
+    }
+}
